18f4112a79756b7838176eda1e73e430ceb85f5a
espadv69 <aitorjob69@gmail.com>
2024-12-31 16:50:38 +0100

    Update: - Refactor button creation to use a declarative approach for better readability

--- 00-hello-world/src/main.jsx
+++ 00-hello-world/src/main.jsx
@@ -5,14 +5,21 @@ const root = ReactDom.createRoot(document.getElementById('root'))
 
 const createButton = ({ text }) => {
   return(
+    <button>{ text }</button> // This is a simple function.
+  )
+}
+
+const Button = ({ text }) => {
+  return(
     <button>{ text }</button>
   )
 }
 
 root.render(
   <React.StrictMode>
-    {createButton({ text: 'button 1'})}
-    {createButton({ text: 'button 2'})}
-    {createButton({ text: 'button 3'})}
+    
+    {createButton({ text: 'button 1'})} {/* Imperative */}
+    <Button text='button 2' /> {/* Declarative */}
+
   </React.StrictMode>
 )
