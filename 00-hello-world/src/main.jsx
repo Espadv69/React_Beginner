493ec8fca23648a7e16eb72bbdec26a1f35f1c3c
espadv69 <aitorjob69@gmail.com>
2024-12-31 16:54:31 +0100

    Update: - Add comments to better experience

--- 00-hello-world/src/main.jsx
+++ 00-hello-world/src/main.jsx
@@ -3,22 +3,28 @@ import ReactDom from 'react-dom'
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 
+// This is a simple function. It's imperative because it's telling the computer how to do something.
 const createButton = ({ text }) => {
-  return(
-    <button>{ text }</button> // This is a simple function.
+  return (
+    <button>
+      {text}
+    </button>
   )
 }
 
+// This is a simple component. It's declarative because it's telling the compute what to do.
 const Button = ({ text }) => {
-  return(
-    <button>{ text }</button>
+  return (
+    <button>
+      {text}
+    </button>
   )
 }
 
 root.render(
   <React.StrictMode>
-    
-    {createButton({ text: 'button 1'})} {/* Imperative */}
+
+    {createButton({ text: 'button 1' })} {/* Imperative */}
     <Button text='button 2' /> {/* Declarative */}
 
   </React.StrictMode>
