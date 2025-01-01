0700e838696053fabada6624b4a98007e04270d5
espadv69 <aitorjob69@gmail.com>
2025-01-01 17:53:59 +0100

    Rename formatUsername function to format for consistency in App component

--- 01-twitter-card/src/components/App.jsx
+++ 01-twitter-card/src/components/App.jsx
@@ -1,27 +1,27 @@
 import { TwitterFollowCard } from './TwitterFollowCard'
 export function App() {
-  const formatUsername = (username) => `@${username}`
+  const format = (username) => `@${username}`
 
   return (
     <section className='tw-followCard-container'>
-      
+
       <TwitterFollowCard
         isFollowing
         fullName={'Espada diaz'}
         username={'aitor69'}
-        formatUsername={formatUsername} />
+        formatUsername={format} />
 
       <TwitterFollowCard
         isFollowing
         fullName={'Domingo gonzalo'}
         username={'terminator'}
-        formatUsername={formatUsername} />
+        formatUsername={format} />
 
       <TwitterFollowCard
         isFollowing
         fullName={'Paco salinas'}
         username={'salinas'}
-        formatUsername={formatUsername} />
+        formatUsername={format} />
 
     </section>
   )
