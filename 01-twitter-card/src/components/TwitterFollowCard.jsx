839ecb82457e765adf00b79032980a29f1b247d7
espadv69 <aitorjob69@gmail.com>
2025-01-01 13:23:24 +0100

    Update: - Refactor TwitterFollowCard component to accept props for name and username

--- 01-twitter-card/src/components/TwitterFollowCard.jsx
+++ 01-twitter-card/src/components/TwitterFollowCard.jsx
@@ -1,17 +1,17 @@
 import '../css/TwitterFollowCard.css'
 
-export function TwitterFollowCard() {
+export function TwitterFollowCard({ fName, username, isFollowing }) {
   return (
     <article className='tw-followCard'>
       <header className='tw-followCard-header'>
         <img
           className='tw-followCard-avatar'
-          src="https://unavatar.io/pepe"
+          src={`https://unavatar.io/${username}`}
           alt="Avatar"
         />
         <div className='tw-followCard-info'>
-          <strong>Pepe Villuela</strong>
-          <span className='tw-followCard-span'>@pepe80</span>
+          <strong>{fName}</strong>
+          <span className='tw-followCard-span'>@{username}</span>
         </div>
       </header>
 
