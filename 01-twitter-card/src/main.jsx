f0fba9cf39253bfb67fbcfbad4288ffba3f652db
espadv69 <aitorjob69@gmail.com>
2025-01-01 13:17:12 +0100

    Update: - fix import twitterfollowcard

--- 01-twitter-card/src/main.jsx
+++ 01-twitter-card/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './components/App.jsx';
+import { App } from './components/TwitterFollowCard.jsx';
 import './main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
