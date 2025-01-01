6826d109dbe67a9288418346d6461d919d419a4b
espadv69 <aitorjob69@gmail.com>
2025-01-01 17:39:00 +0100

    Update TwitterFollowCard prop name from fName to fullName in App component

--- 01-twitter-card/src/components/App.jsx
+++ 01-twitter-card/src/components/App.jsx
@@ -2,9 +2,9 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 export function App() {
   return (
     <section className='tw-followCard-container'>
-      <TwitterFollowCard isFollowing fName={'Espada diaz'} username={'aitor69'} />
-      <TwitterFollowCard isFollowing fName={'Domingo gonzalo'} username={'terminator'} />
-      <TwitterFollowCard isFollowing fName={'Paco salinas'} username={'salinas'} />
+      <TwitterFollowCard isFollowing fullName={'Espada diaz'} username={'aitor69'} />
+      <TwitterFollowCard isFollowing fullName={'Domingo gonzalo'} username={'terminator'} />
+      <TwitterFollowCard isFollowing fullName={'Paco salinas'} username={'salinas'} />
     </section>
   )
 }
