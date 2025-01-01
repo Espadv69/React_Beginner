961c7c27617143634ec7a56d416b542079e40b98
espadv69 <aitorjob69@gmail.com>
2025-01-01 17:48:25 +0100

    Add formatUsername function to App component and pass it to TwitterFollowCard instances

--- 01-twitter-card/src/components/App.jsx
+++ 01-twitter-card/src/components/App.jsx
@@ -1,10 +1,11 @@
 import { TwitterFollowCard } from './TwitterFollowCard'
 export function App() {
+  const formatUsername = (username) => `@${username}`
   return (
     <section className='tw-followCard-container'>
-      <TwitterFollowCard isFollowing fullName={'Espada diaz'} username={'aitor69'} />
-      <TwitterFollowCard isFollowing fullName={'Domingo gonzalo'} username={'terminator'} />
-      <TwitterFollowCard isFollowing fullName={'Paco salinas'} username={'salinas'} />
+      <TwitterFollowCard isFollowing fullName={'Espada diaz'} username={'aitor69'} formatUsername={formatUsername} />
+      <TwitterFollowCard isFollowing fullName={'Domingo gonzalo'} username={'terminator'} formatUsername={formatUsername} />
+      <TwitterFollowCard isFollowing fullName={'Paco salinas'} username={'salinas'} formatUsername={formatUsername} />
     </section>
   )
 }
