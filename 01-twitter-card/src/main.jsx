65092bfa60045ed1e124bc508004e5c8df4a2fc5
espadv69 <aitorjob69@gmail.com>
2025-01-01 13:41:33 +0100

    Refactor main.jsx to use createRoot from react-dom/client and update TwitterFollowCard props to include isFollowing

--- 01-twitter-card/src/main.jsx
+++ 01-twitter-card/src/main.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import createRoot from 'react-dom/client'
 import { TwitterFollowCard } from './components/TwitterFollowCard.jsx';
 import './main.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(
+createRoot.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <section className='tw-followCard-container'>
-      <TwitterFollowCard fName={'Espada diaz'} username={'aitor69'} />
-      <TwitterFollowCard fName={'Domingo gonzalo'} username={'terminator'} />
-      <TwitterFollowCard fName={'Paco salinas'} username={'salinas'} />
+      <TwitterFollowCard isFollowing fName={'Espada diaz'} username={'aitor69'} />
+      <TwitterFollowCard isFollowing fName={'Domingo gonzalo'} username={'terminator'} />
+      <TwitterFollowCard isFollowing fName={'Paco salinas'} username={'salinas'} />
     </section>
   </React.StrictMode>
 )
