5bf469452337a8e37fbedacc93ecec069a48f725
espadv69 <aitorjob69@gmail.com>
2025-01-01 17:37:38 +0100

    Replace TwitterFollowCard component with App component in main.jsx

--- 01-twitter-card/src/main.jsx
+++ 01-twitter-card/src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import { TwitterFollowCard } from './components/TwitterFollowCard.jsx';
+import { App } from './components/App.jsx';
 import './main.css';
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <section className='tw-followCard-container'>
-      <TwitterFollowCard isFollowing fName={'Espada diaz'} username={'aitor69'} />
-      <TwitterFollowCard isFollowing fName={'Domingo gonzalo'} username={'terminator'} />
-      <TwitterFollowCard isFollowing fName={'Paco salinas'} username={'salinas'} />
-    </section>
+    <App />
   </React.StrictMode>
 )
