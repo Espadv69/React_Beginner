d57a18e7c70ba6fea23c519382e72db58dd86b75
espadv69 <aitorjob69@gmail.com>
2025-01-01 17:50:40 +0100

    Refactor TwitterFollowCard instances for improved readability

--- 01-twitter-card/src/components/App.jsx
+++ 01-twitter-card/src/components/App.jsx
@@ -1,11 +1,28 @@
 import { TwitterFollowCard } from './TwitterFollowCard'
 export function App() {
   const formatUsername = (username) => `@${username}`
+
   return (
     <section className='tw-followCard-container'>
-      <TwitterFollowCard isFollowing fullName={'Espada diaz'} username={'aitor69'} formatUsername={formatUsername} />
-      <TwitterFollowCard isFollowing fullName={'Domingo gonzalo'} username={'terminator'} formatUsername={formatUsername} />
-      <TwitterFollowCard isFollowing fullName={'Paco salinas'} username={'salinas'} formatUsername={formatUsername} />
+      
+      <TwitterFollowCard
+        isFollowing
+        fullName={'Espada diaz'}
+        username={'aitor69'}
+        formatUsername={formatUsername} />
+
+      <TwitterFollowCard
+        isFollowing
+        fullName={'Domingo gonzalo'}
+        username={'terminator'}
+        formatUsername={formatUsername} />
+
+      <TwitterFollowCard
+        isFollowing
+        fullName={'Paco salinas'}
+        username={'salinas'}
+        formatUsername={formatUsername} />
+
     </section>
   )
 }
