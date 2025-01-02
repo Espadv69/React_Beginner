4d4959e63bd5a0b2b8a568e60dd6bd508e5445bd
espadv69 <aitorjob69@gmail.com>
2025-01-02 13:04:29 +0100

    Update: - Add username state management and button to update username in App component

--- 01-twitter-card/src/components/App.jsx
+++ 01-twitter-card/src/components/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react'
 import { TwitterFollowCard } from './TwitterFollowCard'
+
 export function App() {
   // We can pass the props in the same way as we did in the previous example.
   const aitor = { username: 'aitor69' } // Bad practice
+
+  const [username, setUsername] = useState('terminator._')
+  const handleUsername = () => setUsername('pabloelmotos')
 
   return (
     <section className='tw-followCard-container'>
@@ -10,10 +15,13 @@ export function App() {
         Aitor Job { /* Children */ }
       </TwitterFollowCard>
 
-      <TwitterFollowCard username={'terminator'}>
+      <TwitterFollowCard username={username}>
         Terminator Bom { /* Children */ }
       </TwitterFollowCard>
 
+      <button onClick={handleUsername}>
+        Cambio de nombre
+      </button>
     </section>
   )
 
