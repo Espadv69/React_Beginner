f186df186ba70798a242a469dd3ef35a04bb73fd
espadv69 <aitorjob69@gmail.com>
2025-01-02 12:40:40 +0100

    Update: - Fix TwitterFollowCard to set isFollowing prop to false for Terminator

--- 01-twitter-card/src/components/App.jsx
+++ 01-twitter-card/src/components/App.jsx
@@ -10,7 +10,7 @@ export function App() {
         Aitor Job { /* Children */ }
       </TwitterFollowCard>
 
-      <TwitterFollowCard isFollowing username={'terminator'}>
+      <TwitterFollowCard isFollowing={false} username={'terminator'}>
         Terminator Bom { /* Children */ }
       </TwitterFollowCard>
 
