c2efdd0baa4894459a8fcdfc4ac37d69ed25f751
espadv69 <aitorjob69@gmail.com>
2025-01-02 12:58:55 +0100

    Update: - Refactor conditional rendering in TwitterFollowCard component for improved readability

--- 01-twitter-card/src/components/TwitterFollowCard.jsx
+++ 01-twitter-card/src/components/TwitterFollowCard.jsx
@@ -7,11 +7,16 @@ export function TwitterFollowCard({ children, username }) {
   const [isFollowing, setIsFollowing] = useState(false)
 
   // Conditional rendering
-  const text = isFollowing ? 'Following' : 'Follow'
-  const classButton = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+  const text = isFollowing
+    ? 'Following'
+    : 'Follow'
+
+  const classButton = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
 
   const handleClick = () => setIsFollowing(!isFollowing) // Internal state
-  
+  console.log(isFollowing)
   return (
     <article className='tw-followCard'>
       <header className='tw-followCard-header'>
@@ -33,5 +38,5 @@ export function TwitterFollowCard({ children, username }) {
       </aside>
     </article>
   )
-  
+
 }
