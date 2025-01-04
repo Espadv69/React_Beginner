5f3c6b506ccfa1dbbbabcf16a8c3927f8076fbac
espadv69 <aitorjob69@gmail.com>
2025-01-04 12:35:14 +0100

    Implement winner checking logic in board.js using WINNER_COMBINATIONS constant

---
+++ 02-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,15 @@
+import { WINNER_COMBINATIONS } from '../constants.js' // Import WINNER_COMBINATIONS constant
+
+export const checkWinnerFrom = (boardToCheck) => {
+
+  for (const combo of WINNER_COMBINATIONS) {
+    const [a, b, c] = combo // Destructure the combo
+    if (
+      boardToCheck[a] && // Check if the value is not null
+      boardToCheck[a] === boardToCheck[b] && // Check if the values are the same
+      boardToCheck[a] === boardToCheck[c] // Check if the values are the same
+    ) {
+      return boardToCheck[a] // Return the winner
+    }
+  }
+}
