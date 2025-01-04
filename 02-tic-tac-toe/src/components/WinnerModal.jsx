2b406eae241a2434fd6f127cfe4236be8d51c200
espadv69 <aitorjob69@gmail.com>
2025-01-04 12:48:13 +0100

    Add WinnerModal component with reset functionality and winner display

---
+++ 02-tic-tac-toe/src/components/WinnerModal.jsx
@@ -0,0 +1,21 @@
+import { Square } from './Square'
+export function WinnerModal({ winner, resetGame }) { // Add resetGame to the props
+  if (winner === null) return null
+
+  const winnerText = winner === false ? 'Draw!' : `Winner: ${winner}`
+  return (
+    <section className='winner'>
+      <div className='text'>
+        <h2>{winnerText}</h2>
+
+        <header className='win'>
+          {winner && <Square>{winner}</Square>} {/* Add Square component to display the winner */}
+        </header>
+
+        <footer>
+          <button onClick={resetGame}>Restart</button> {/* Add onClick event to the button */}
+        </footer>
+      </div>
+    </section>
+  )
+}
