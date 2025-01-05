43a10c07fd22093d8ac3af63edd4f92fd626c2cf
espadv69 <aitorjob69@gmail.com>
2025-01-05 20:05:01 +0100

    feat(Counter_useState): add increment and decrement functionality

--- 03-hooks/src/component/Counter_useState.jsx
+++ 03-hooks/src/component/Counter_useState.jsx
@@ -1,5 +1,22 @@
 import { useState } from 'react'
 
 export const Counter_useState = () => {
+  const [counter, setCounter] = useState(0)
 
+  function sum() {
+    setCounter(counter + 1)
+  }
+
+  function subtract() {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <div className="container">
+        <button onClick={sum}>Sum</button>
+        <button onClick={subtract}>Subtract</button>
+      </div>
+    </>
+  )
 }
