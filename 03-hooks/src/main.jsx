105c8f5a3c1122e0b6c3745a116b7a1da6a7926e
espadv69 <aitorjob69@gmail.com>
2025-01-05 20:23:09 +0100

    feat(main): integrate Counter_useState component into main application

--- 03-hooks/src/main.jsx
+++ 03-hooks/src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+
+import { Counter_useState } from './component/Counter_useState.jsx'
 import './css/index.css'
 
 const root = ReactDom.createRoot(document.getElementById('root'))
@@ -7,5 +9,7 @@ const root = ReactDom.createRoot(document.getElementById('root'))
 root.render(
   <>
     <h1 className="h1-title">Hooks</h1>
+
+    <Counter_useState />
   </>
 )
