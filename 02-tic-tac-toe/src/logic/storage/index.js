3aae8ee9807a58030772e1ea0a1b515fa1f51d94
espadv69 <aitorjob69@gmail.com>
2025-01-05 12:50:19 +0100

    Rename saveGame and resetGame functions to saveGameFromStorage and resetGameFromStorage for clarity

--- 02-tic-tac-toe/src/logic/storage/index.js
+++ 02-tic-tac-toe/src/logic/storage/index.js
@@ -1,9 +1,9 @@
-const saveGame = ({ newBoard, newTurn }) => {
+export const saveGameFromStorage = ({ newBoard, newTurn }) => {
   window.localStorage.setItem('board', JSON.stringify(newBoard))
   window.localStorage.setItem('turn', newTurn)
 }
 
-const resetGame = () => {
+export const resetGameFromStorage = () => {
   window.localStorage.removeItem('board')
   window.localStorage.removeItem('turn')
 }
