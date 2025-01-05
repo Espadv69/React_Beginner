ca070b8626949327170cb1d288e0de49589300c9
espadv69 <aitorjob69@gmail.com>
2025-01-05 20:47:08 +0100

    feat(ToDoList): implement task management functionality with input and display

--- 03-hooks/src/component/ToDoList.jsx
+++ 03-hooks/src/component/ToDoList.jsx
@@ -2,5 +2,26 @@ import { useState, useEffect } from 'react'
 import '../css/Components.css'
 
 export const ToDoList = () => {
-  
+  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+  return (
+    <div>
+      <h1>Tasks List</h1>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Write a task"
+      />
+
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
