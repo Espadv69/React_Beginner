89ffa6d7f30ce5ceb7dcd25d9d2dd0fc8e5f1e9c
espadv69 <aitorjob69@gmail.com>
2025-01-06 15:00:04 +0100

    feat(Form): implement initial Form component with state management

---
+++ 03-hooks/src/component/Form.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+import '../css/Components.css'
+
+export const Form = () => {
+  const [email, setEmail] = useState('')
+  const [error, setError] = useState('')
+}
