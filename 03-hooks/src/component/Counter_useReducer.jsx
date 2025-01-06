0ec6cf970d392fc68a3f41915e61e8f53e1b220e
espadv69 <aitorjob69@gmail.com>
2025-01-06 23:33:39 +0100

    feat(Counter): implement useReducer logic for counter state management

--- 03-hooks/src/component/Counter_useReducer.jsx
+++ 03-hooks/src/component/Counter_useReducer.jsx
@@ -1,11 +1,28 @@
 import { useReducer } from 'react'
 import '../css/Components.css'
 
+const initialCounter = { count: 0 }
+
+const counter = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1}
+    case ' reset':
+      return initialCounter
+    default:
+      throw new Error('Unknow action')
+  }
+}
+
 export const Counter_useReducer = () => {
+  const [state, dispatch] = useReducer(counter, initialCounter)
+
   return (
     <div>
       <h1 className="h1-title">Counter useReducer</h1>
-      <p className="p-counter"></p>
+      <p className="p-counter">{state.count}</p>
       <div className='buttons-container'>
         <button className='btn'>Increment</button>
         <button className='btn'>Decrement</button>
