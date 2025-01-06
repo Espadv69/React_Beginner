a6d715702837d5b21845d726918b5f2b9836a485
espadv69 <aitorjob69@gmail.com>
2025-01-06 15:00:20 +0100

    Structure of form

--- 03-hooks/src/component/Form.jsx
+++ 03-hooks/src/component/Form.jsx
@@ -4,4 +4,24 @@ import '../css/Components.css'
 export const Form = () => {
   const [email, setEmail] = useState('')
   const [error, setError] = useState('')
+
+  return (
+    <div>
+      <h1 className="h1-title">Form</h1>
+      <form className="form">
+        <label className="label-form">Email:</label>
+        <input
+          className="input-form"
+          type="text"
+        />
+        <button
+          className="button-form"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+      {/* todo: error message */}
+    </div>
+  )
 }
