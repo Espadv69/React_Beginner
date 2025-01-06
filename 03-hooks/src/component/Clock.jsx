7bcc9e5b09509c2d7bb1965fc9efe14de80f70f6
espadv69 <aitorjob69@gmail.com>
2025-01-06 12:25:17 +0100

    feat(Clock): add date update functionality with useEffect

--- 03-hooks/src/component/Clock.jsx
+++ 03-hooks/src/component/Clock.jsx
@@ -3,4 +3,12 @@ import '../css/Components.css'
 
 export const Clock = () => {
   const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+
+    return () => clearInterval(intervalId)
+  }, [])
 }
