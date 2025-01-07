1264131c1e82b7adf3b7e6920b7170cf2244b304
espadv69 <aitorjob69@gmail.com>
2025-01-07 22:12:10 +0100

    Fetch and display users in UserList component

--- 04-hooks-02/src/components/UserList.jsx
+++ 04-hooks-02/src/components/UserList.jsx
@@ -1,7 +1,16 @@
 import { useState, useEffect } from 'react'
 
 export const UserList = () => {
+  const [users, setUsers] = useState([])
+  const [loanding, setLoanding] = useState(true)
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((e) => console.error('Error fetching users:', error))
+      .finally(() => setLoanding(false))
+  }, [])
 
   return (
     <div>
