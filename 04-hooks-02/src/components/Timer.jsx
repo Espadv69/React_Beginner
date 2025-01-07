7aae69a844c728ac7c3badf879d635d8ace90c12
espadv69 <aitorjob69@gmail.com>
2025-01-07 15:18:24 +0100

    Implement state management for Timer component with seconds and active status

--- 04-hooks-02/src/components/Timer.jsx
+++ 04-hooks-02/src/components/Timer.jsx
@@ -3,9 +3,12 @@ import { useState, useEffect } from 'react'
 import '../css/Components.css'
 
 export const Timer = () => {
+  const [seconds, setSeconds] = useState(0)
+  const [isActive, setIsActive] = useState(true)
+
   return (
     <div>
-      <p className="p-timer"></p>
+      <p className="p-timer">{}</p>
       <div className="btns-timer">
         <button className="btn-timer">{}</button>
         <button className="btn-timer">Reset</button>
