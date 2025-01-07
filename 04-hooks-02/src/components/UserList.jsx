5eed889226d45f69d71103d9e6c079d165ce6a81
espadv69 <aitorjob69@gmail.com>
2025-01-07 22:07:46 +0100

    Add UserList component with initial structure

---
+++ 04-hooks-02/src/components/UserList.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+
+export const UserList = () => {
+
+
+  return (
+    <div>
+      <h1 className="h1-title">User List</h1>
+      {}
+      {}
+    </div>
+  )
+}
