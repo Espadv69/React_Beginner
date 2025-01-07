a1573ceae9595ab72775b851340acaeb7370d254
espadv69 <aitorjob69@gmail.com>
2025-01-07 13:43:50 +0100

    update Clock component to display the current time using useState and useEffect

--- 04-hooks-02/src/components/Clock.jsx
+++ 04-hooks-02/src/components/Clock.jsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from 'react'
 
 export const Clock = () => {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const intervalTime = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+
+    return () => clearInterval(intervalTime)
+  }, [])
 
   return (
     <div className="clock">
-      <p>{}</p>
+      <p>{date.toLocaleTimeString()}</p>
     </div>
   )
 }
