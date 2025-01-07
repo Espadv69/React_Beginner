7910e73629eb19c652026fa7f9af0284d57ccb9b
espadv69 <aitorjob69@gmail.com>
2025-01-07 13:26:06 +0100

    update main.jsx to render a container with a title for React Hooks

--- 04-hooks-02/src/main.jsx
+++ 04-hooks-02/src/main.jsx
@@ -5,6 +5,8 @@ import './css/main.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render (
-  <h1>Hello, React!</h1>
+root.render(
+  <div className="container-index">
+    <h1 className="h1-title">React Hooks</h1>
+  </div>
 )
