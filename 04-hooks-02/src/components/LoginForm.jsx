cdf04b58d18f41a2578864e7818b38cca5c95b5d
espadv69 <aitorjob69@gmail.com>
2025-01-08 11:54:31 +0100

    Implement LoginForm component structure with form inputs and submit button

--- 04-hooks-02/src/components/LoginForm.jsx
+++ 04-hooks-02/src/components/LoginForm.jsx
@@ -1,5 +1,14 @@
 import { useState } from 'react'
 
 export const LoginForm = () => {
-  
+  return (
+    <div>
+      <h1 className="h1-title">Login-Form</h1>
+      <form className="form-login">
+        <input className="input-form-login" type="text" />
+        <input className="input-form-login" type="text" />
+        <button className="button-form-login">Submit</button>
+      </form>
+    </div>
+  )
 }
