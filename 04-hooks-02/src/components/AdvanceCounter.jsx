0ee22b698ef88e1c63ab4d6fbe6d5294766dedd2
espadv69 <aitorjob69@gmail.com>
2025-01-08 19:26:56 +0100

    Add reducer function for counter actions in AdvanceCounter component

--- 04-hooks-02/src/components/AdvanceCounter.jsx
+++ 04-hooks-02/src/components/AdvanceCounter.jsx
@@ -2,7 +2,18 @@ import { useReducer } from "react"
 
 const initialCounter = { count: 0 }
 
-
+const calculator = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    case 'reset':
+      return initialCounter
+    default:
+      throw new Error('Unknow type action')
+  }
+}
 
 export const AdvanceCounter = () => {
   return (
