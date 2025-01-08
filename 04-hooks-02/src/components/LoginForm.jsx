86b8427c30c54b62c089c1ded2c37fc0f8b43c94
espadv69 <aitorjob69@gmail.com>
2025-01-08 12:04:48 +0100

    Add form submission handling and input validation to LoginForm component

--- 04-hooks-02/src/components/LoginForm.jsx
+++ 04-hooks-02/src/components/LoginForm.jsx
@@ -3,13 +3,35 @@ import { useState } from 'react'
 export const LoginForm = () => {
   const [user, setUser] = useState('')
   const [password, setPassword] = useState('')
-  
+
+  const handleSubmit = (event) => {
+    if (user !== '' && password !== '') {
+      event.preventDefault()
+      alert(`User: (${user}) Password: (${password})`)
+      setUser('')
+      setPassword('')
+    } else {
+      event.preventDefault()
+      alert('Empty input(s)')
+    }
+  }
+
   return (
     <div>
       <h1 className="h1-title">Login-Form</h1>
-      <form className="form-login">
-        <input className="input-form-login" type="text" />
-        <input className="input-form-login" type="text" />
+      <form className="form-login" onSubmit={handleSubmit}>
+        <input
+          className="input-form-login"
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <input
+          className="input-form-login"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button className="button-form-login">Submit</button>
       </form>
     </div>
