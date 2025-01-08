e7f1aeed3c55a4ad223e7f03e55433b1b788b66c
espadv69 <aitorjob69@gmail.com>
2025-01-08 19:26:46 +0100

    Implement basic structure for AdvanceCounter component with buttons

--- 04-hooks-02/src/components/AdvanceCounter.jsx
+++ 04-hooks-02/src/components/AdvanceCounter.jsx
@@ -1,3 +1,16 @@
+import { useReducer } from "react"
+
+const initialCounter = { count: 0 }
+
+
+
 export const AdvanceCounter = () => {
-  
+  return (
+    <div>
+      <p>{ }</p>
+      <button>Increment</button>
+      <button>Decrement</button>
+      <button>Reset</button>
+    </div>
+  )
 }
