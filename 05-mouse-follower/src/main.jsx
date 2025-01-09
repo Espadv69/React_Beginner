7fcbb082fd79a6cd06308ad2a16c66765f8e3762
espadv69 <aitorjob69@gmail.com>
2025-01-09 13:21:23 +0100

    feat: wrap MouseFollower component in React.StrictMode for improved development checks

--- 05-mouse-follower/src/main.jsx
+++ 05-mouse-follower/src/main.jsx
@@ -7,7 +7,7 @@ import './css/index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <div>
+  <React.StrictMode>
     <MouseFollower />
-  </div>
+  </React.StrictMode>
 )
