57f2e2cae3265a10b85fd1c9109bb49937bb2653
espadv69 <aitorjob69@gmail.com>
2025-01-10 19:28:27 +0100

    feat: implement AboutPage component with navigation to Home

---
+++ 06-espadv-router/src/pages/About.jsx
@@ -0,0 +1,22 @@
+import { navigateTo } from '../components/App.jsx'
+
+export function AboutPage() {
+  return (
+    <>
+      <h1 className="h1-title">About</h1>
+      <div className="div-about">
+        <img
+          src="https://avatars.githubusercontent.com/u/162121948?s=96&v=4"
+          alt="Espadv"
+          className="img-spa"
+        />
+        <p className="p-spa">
+          Hello!, I'm Espadv and i'm creating a clon of React router.
+        </p>
+      </div>
+      <button onClick={() => navigateTo('/')} className="a-spa">
+        Home
+      </button>
+    </>
+  )
+}
