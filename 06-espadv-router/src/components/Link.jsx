f1cfe391bb0ca42ed9b4639b24fedca35eac6b90
espadv69 <aitorjob69@gmail.com>
2025-01-10 20:08:46 +0100

    feat: integrate BUTTONS constant for primary click event detection in Link component

--- 06-espadv-router/src/components/Link.jsx
+++ 06-espadv-router/src/components/Link.jsx
@@ -1,4 +1,4 @@
-import { EVENTS } from '../consts.js'
+import { EVENTS, BUTTONS } from '../consts.js'
 
 export function navigateTo(href) {
   window.history.pushState({}, '', href)
@@ -9,7 +9,7 @@ export function navigateTo(href) {
 
 export function Link({ target, to, ...props }) {
   const handleClick = (e) => {
-    const isMainEvent = e.button === 0 // Left click
+    const isMainEvent = e.button === BUTTONS.primary
     const isModifiedEvent =
       e.metaKey || e.ctrlKey || e.shiftKey || e.altKey || e.defaultPrevented
     const isManagableEvent = target === undefined || target === '_self'
