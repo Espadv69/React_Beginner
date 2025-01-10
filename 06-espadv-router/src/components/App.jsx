20b111540f8bc2317aa7e0e862bc8c991f1bbe18
espadv69 <aitorjob69@gmail.com>
2025-01-10 12:48:34 +0100

    style: update HomePage and AboutPage components with CSS classes for improved styling

--- 06-espadv-router/src/components/App.jsx
+++ 06-espadv-router/src/components/App.jsx
@@ -3,9 +3,13 @@ import '../css/App.css'
 function HomePage() {
   return (
     <>
-      <h1>Home</h1>
-      <p>This is an example page for creating a React Router from scratch.</p>
-      <a href="/about">About us</a>
+      <h1 className="h1-title">Home</h1>
+      <p className="p-spa">
+        This is an example page for creating a React Router from scratch.
+      </p>
+      <a href="/about" className="a-spa">
+        About us
+      </a>
     </>
   )
 }
@@ -13,15 +17,19 @@ function HomePage() {
 function AboutPage() {
   return (
     <>
-      <h1>About</h1>
-      <div>
-        <p>Hello!, I'm Espadv and i'm creating a clon of React router.</p>
+      <h1 className="h1-title">About</h1>
+      <div className="div-about">
+        <p className="p-spa">
+          Hello!, I'm Espadv and i'm creating a clon of React router.
+        </p>
         <img
           src="https://avatars.githubusercontent.com/u/162121948?s=96&v=4"
           alt="Espadv"
         />
       </div>
-      <a href="/">Home</a>
+      <a href="/" className="a-spa">
+        Home
+      </a>
     </>
   )
 }
