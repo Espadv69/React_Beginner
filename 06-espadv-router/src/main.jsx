9226ef438ead5fce9ef982aea8d8c0966a698620
espadv69 <aitorjob69@gmail.com>
2025-01-10 12:24:17 +0100

    render App component in main.jsx

--- 06-espadv-router/src/main.jsx
+++ 06-espadv-router/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { App } from './components/App.jsx'
 import './css/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render (
+root.render(
   <React.StrictMode>
     <h1>hello, world!</h1>
+    <App />
   </React.StrictMode>
 )
