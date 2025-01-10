7daa9fe485caf3fa4e4fd5ea92227685b67150b3
espadv69 <aitorjob69@gmail.com>
2025-01-10 19:45:24 +0100

    feat: implement Link component with click handler for navigation

--- 06-espadv-router/src/components/Link.jsx
+++ 06-espadv-router/src/components/Link.jsx
@@ -6,3 +6,9 @@ export function navigateTo(href) {
   const navEvent = new Event(EVENTS.PUSHSTATE)
   window.dispatchEvent(navEvent)
 }
+
+export function Link({ target, to, ...props }) {
+  const handleClick = (e) => {}
+
+  return <a onClick={handleClick}></a>
+}
