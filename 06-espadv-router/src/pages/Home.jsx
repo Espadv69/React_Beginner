da1c5770071c611baf72a70238a779adc9a43665
espadv69 <aitorjob69@gmail.com>
2025-01-10 19:41:57 +0100

    feat: update className for button in HomePage for consistent styling

--- 06-espadv-router/src/pages/Home.jsx
+++ 06-espadv-router/src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <button
         type="button"
         onClick={() => navigateTo('/about')}
-        className="a-spa"
+        className="button-spa"
       >
         About us
       </button>
