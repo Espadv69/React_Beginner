123df14f0134c31190c5d05ec3938d38ff897647
espadv69 <aitorjob69@gmail.com>
2025-01-10 12:47:00 +0100

    add AboutPage component and render it in App

--- 06-espadv-router/src/components/App.jsx
+++ 06-espadv-router/src/components/App.jsx
@@ -10,10 +10,27 @@ function HomePage() {
   )
 }
 
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <div>
+        <p>Hello!, I'm Espadv and i'm creating a clon of React router.</p>
+        <img
+          src="https://avatars.githubusercontent.com/u/162121948?s=96&v=4"
+          alt="Espadv"
+        />
+      </div>
+      <a href="/">Home</a>
+    </>
+  )
+}
+
 export const App = () => {
   return (
     <div>
       <HomePage />
+      <AboutPage />
     </div>
   )
 }
