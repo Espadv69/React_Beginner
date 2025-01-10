2d58a9a05a46630221445af80dca36da7422acf2
espadv69 <aitorjob69@gmail.com>
2025-01-10 12:43:22 +0100

    update HomePage component to include description and link to About page

--- 06-espadv-router/src/components/App.jsx
+++ 06-espadv-router/src/components/App.jsx
@@ -1,7 +1,15 @@
 import '../css/App.css'
+
 function HomePage() {
-  return <h1>Home Page</h1>
+  return (
+    <>
+      <h1>Home</h1>
+      <p>This is an example page for creating a React Router from scratch.</p>
+      <a href="/about">About us</a>
+    </>
+  )
 }
+
 export const App = () => {
   return (
     <div>
