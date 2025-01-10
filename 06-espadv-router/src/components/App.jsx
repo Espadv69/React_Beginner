9262596dd9802624ac854663cbc8469ae239f40e
espadv69 <aitorjob69@gmail.com>
2025-01-10 19:28:23 +0100

    feat: import AboutPage component into App and remove inline AboutPage function

--- 06-espadv-router/src/components/App.jsx
+++ 06-espadv-router/src/components/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 
 import { HomePage } from '../pages/Home.jsx'
+import { AboutPage } from '../pages/About.jsx'
 import { EVENTS } from '../consts.js'
 import '../css/App.css'
 
@@ -11,29 +12,6 @@ export function navigateTo(href) {
   // Create a personalized event
   const navEvent = new Event(EVENTS.PUSHSTATE)
   window.dispatchEvent(navEvent)
-}
-
-
-
-function AboutPage() {
-  return (
-    <>
-      <h1 className="h1-title">About</h1>
-      <div className="div-about">
-        <img
-          src="https://avatars.githubusercontent.com/u/162121948?s=96&v=4"
-          alt="Espadv"
-          className="img-spa"
-        />
-        <p className="p-spa">
-          Hello!, I'm Espadv and i'm creating a clon of React router.
-        </p>
-      </div>
-      <button onClick={() => navigateTo('/')} className="a-spa">
-        Home
-      </button>
-    </>
-  )
 }
 
 export const App = () => {
